287f1cec5758bd044749377018130ee9774c93f8
Avery Burns <a.aburns95@gmail.com>
2020-09-27 22:39:07 -0500

    added axios & styled dependencies, axios get request

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,27 @@
-// Write your Character component here
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+
+function Character() {
+    const [char, setChar] = useState([]);
+
+    // API http https://swapi.dev/api/people/
+    useEffect(() => {
+        axios
+            .get("https://swapi.dev/api/people/")
+            .then(res => {
+                console.log(res.data.results)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    })
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Character;
